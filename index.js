0c823b24b7e831f0b87bd613ba98394ed1e6e7ba
proloxin <654007889@qq.com>
2020-04-30 16:54:57 +0800

    第一次部署

---
+++ index.js
@@ -0,0 +1,19 @@
+const Koa = require('koa')
+const app = new Koa()
+const route = require('koa-route')
+const serve = require('koa-static')
+const path = require("path")
+
+const home = serve(path.join(__dirname)+'/public/')
+
+const hello = ctx =>{
+    ctx.body = 'hello world'
+}
+
+app.use(home)
+app.use(route.get('/',hello))
+
+app.listen(3000,()=>{
+    console.log('sever running at port 3000');
+    
+})
