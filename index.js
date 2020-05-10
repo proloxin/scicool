421a660466885bf5f4a2838b00a00aa9993db26c
proloxin <654007889@qq.com>
2020-05-10 17:49:18 +0800

    增加手机支持

--- index.js
+++ index.js
@@ -10,7 +10,10 @@ let subPosts = require('./api/subPosts')
 const {connect,initSchemas} = require('./database/init')
 const cors = require('koa-cors')
 const home = serve(path.join(__dirname)+'/public/')
-app.use(home)
+const mobi = serve(path.join(__dirname)+'/mobile/')
+const fs = require('fs')
+// app.use(home)
+// app.use(mobi)
 
 app.use(cors({
     origin: function (ctx) {
@@ -23,6 +26,31 @@ app.use(cors({
     allowHeaders: ['Content-Type', 'Authorization', 'Accept'],
 }))
 app.use(bodyparser())
+
+
+router.get("/",async(ctx)=> {
+    let machine = getMachine(ctx.request);
+    if(machine=='PC'){
+        app.use(home)
+        ctx.set('Content-Type', "text/html;charset=utf-8")
+        ctx.body = fs.createReadStream('./public/index.html')
+    }else{
+        app.use(mobi)
+        ctx.set('Content-Type', "text/html;charset=utf-8")
+        ctx.body = fs.createReadStream('./mobile/index.html')
+    }
+    
+})
+
+function getMachine(req) {
+    var deviceAgent = req.headers["user-agent"].toLowerCase();
+    var agentID = deviceAgent.match(/(iphone|ipod|ipad|android)/);
+    if (agentID) {
+        return "Mobile";
+    } else {
+        return "PC";
+    }
+}
 
 ;(async()=>{
     await connect()
